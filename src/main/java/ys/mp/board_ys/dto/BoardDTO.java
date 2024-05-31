1c77ba11f6038b159f38428244ac20ecb8290626
박한비 <146406303+gganb@users.noreply.github.com>
2024-06-01 03:39:02 +0900

    Update BoardDTO.java
    프로젝트연결

--- src/main/java/ys/mp/board_ys/dto/BoardDTO.java
+++ src/main/java/ys/mp/board_ys/dto/BoardDTO.java
@@ -1,29 +1,62 @@
-package ys.mp.board_ys.dto;
+package ys.mp.board_ys.entitiy;
 
-import lombok.*;
-import ys.mp.board_ys.entitiy.BoardEntity;
+import jakarta.persistence.*;
+import lombok.Getter;
+import lombok.Setter;
+import org.springframework.data.annotation.CreatedDate;
+import org.springframework.data.annotation.LastModifiedDate;
+import ys.mp.board_ys.dto.BoardDTO;
 
 import java.time.LocalDateTime;
+import java.util.ArrayList;
+import java.util.List;
 
 @Getter
 @Setter
-@ToString
-@NoArgsConstructor
-@AllArgsConstructor
+@Entity
+@Table(name = "board_table")
+public class BoardEntity {
 
-public class BoardDTO {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
+    @Column(nullable = false)
     private String boardWriter;
+
+    @Column
     private String boardTitle;
+
+    @Column(length = 500)
     private String boardContents;
+
+    @CreatedDate
+    @Column(name = "created_at", updatable = false)
     private LocalDateTime createdAt;
 
-    public static BoardDTO toBoardDTO(BoardEntity boardEntity) {
-        BoardDTO boardDTO = new BoardDTO();
-        boardDTO.setId(boardEntity.getId());
-        boardDTO.setBoardWriter(boardEntity.getBoardWriter());
-        boardDTO.setBoardTitle(boardEntity.getBoardTitle());
-        boardDTO.setBoardContents(boardEntity.getBoardContents());
-        return boardDTO;
+    @LastModifiedDate
+    @Column(name = "updated_at")
+    private LocalDateTime updatedAt;
+
+    @ManyToOne
+    @JoinColumn(name = "project_id") // 외래 키로 사용될 필드
+    private Project project; // 프로젝트와의 관계
+
+    public static BoardEntity toSaveEntity(BoardDTO boardDTO, Project project) {
+        BoardEntity boardEntity = new BoardEntity();
+        boardEntity.setBoardWriter(boardDTO.getBoardWriter());
+        boardEntity.setBoardTitle(boardDTO.getBoardTitle());
+        boardEntity.setBoardContents(boardDTO.getBoardContents());
+        boardEntity.setProject(project); // 프로젝트 설정
+        return boardEntity;
+    }
+
+    public static BoardEntity toUpdateEntity(BoardDTO boardDTO, Project project) {
+        BoardEntity boardEntity = new BoardEntity();
+        boardEntity.setId(boardDTO.getId());
+        boardEntity.setBoardWriter(boardDTO.getBoardWriter());
+        boardEntity.setBoardTitle(boardDTO.getBoardTitle());
+        boardEntity.setBoardContents(boardDTO.getBoardContents());
+        boardEntity.setProject(project); // 프로젝트 설정
+        return boardEntity;
     }
 }
